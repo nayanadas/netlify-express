684158a269ce2334d75ff96451a81bf6b6ee991b
nayanadas <nayanadas.amrita@gmail.com>
2021-11-15 15:58:35 +0530

    Update app.js

--- express/app.js
+++ express/app.js
@@ -13,6 +13,7 @@ router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
